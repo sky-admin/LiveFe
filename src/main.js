16445dc7d5e7e2cb016e17afd418fdb49fc36226
sky-ADMIN <546485299@qq.com>
2017-04-17 21:19:26 +0800

    add vue-resource

--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueResource from 'vue-resource'
 
 Vue.config.productionTip = false
+
+Vue.use(VueResource);
+Vue.http.options.emulateJSON = true;
+Vue.http.options.credentials = true;
 
 /* eslint-disable no-new */
 new Vue({
