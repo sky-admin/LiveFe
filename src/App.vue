e03cb76324d36c1d65fba962984340648d5ceb72
echaoo <zmk181@gmail.com>
2017-04-17 22:47:21 +0800

    login 暂存

--- src/App.vue
+++ src/App.vue
@@ -11,5 +11,7 @@ export default {
 </script>
 
 <style>
-
+  body {
+    margin: 0;
+  }
 </style>
---
+++ src/components/login/Login.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="login">
+    <div class="login-panel">
+
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'login',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less" scoped>
+  .login {
+    background-color: #eaeaea;
+    width: 100vw;
+    height: 100vh;
+
+    .login-panel {
+      background-color: #fff;
+      width: 300px;
+    }
+  }
+</style>
