83b4a5ca1cf8a7adccef1a91fad86b31ff2f9d34
sky-ADMIN <546485299@qq.com>
2017-05-15 16:22:00 +0800

    修复一个因改名造成的bug， 添加具体直播页面组件

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '../components/login/Login.vue'
 import Register from '../components/register/Register.vue'
-import Index from '../components/index/index.vue'
+import Index from '../components/index/Index.vue'
+import Live from '../components/live/Live.vue'
 // import Hello from '@/components/Hello'
 
 Vue.use(Router)
@@ -23,6 +24,11 @@ export default new Router({
       path: '/reg',
       name: 'reg',
       component: Register
+    },
+    {
+      path: '/live/:id',
+      name: 'live',
+      component: Live
     }
   ]
 })
