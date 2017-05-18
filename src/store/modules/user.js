4f33218b8f33737b2c105605e2f85d859467ce74
sky-ADMIN <546485299@qq.com>
2017-05-18 21:21:13 +0800

    处理用户刷新网页后从local Storage取数据的相关逻辑

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -4,12 +4,16 @@
 import * as types from '../mutation-types'
 
 const state = {
-  accessToken: ''
+  accessToken: '',
+  userId: '',
+  userData: {}
 };
 
 // getters
 const getters = {
-  token: state => state.token
+  accessToken: state => state.accessToken,
+  userId: state => state.userId,
+  userData: state => state.userData
 };
 
 // actions
@@ -20,7 +24,9 @@ const actions = {
         (res) => {
           if (res.ok === true) {
             localStorage.setItem('accessToken', res.data.id);
+            localStorage.setItem('userId', res.data.userId);
             context.commit(types.SET_TOKEN, {token: res.data.id});
+            context.commit(types.SET_USERID, {userId: res.data.userId});
             resolve();
           }
           else {
@@ -32,6 +38,24 @@ const actions = {
         }
       )
     })
+  },
+  getUserData (context, promise) {
+    return new Promise((resolve, reject) => {
+      promise.then(
+        (res) => {
+          console.log(res.data)
+        }
+      )
+    })
+  },
+  init (context, {path, router}) {
+    let userId = localStorage.getItem('userId');
+    let accessToken = localStorage.getItem('accessToken');
+    if (userId) {
+      context.commit(types.SET_TOKEN, {token: accessToken});
+      context.commit(types.SET_USERID, {userId: userId});
+      router.push(path);
+    }
   }
 };
 
@@ -39,6 +63,9 @@ const actions = {
 const mutations = {
   [types.SET_TOKEN] (state, {token}) {
     state.accessToken = token
+  },
+  [types.SET_USERID] (state, {userId}) {
+    state.userId = userId
   }
 };
 
