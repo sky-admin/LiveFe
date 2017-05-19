4db70b67c21ebb15af5a5504303726d1efab1b35
sky-ADMIN <546485299@qq.com>
2017-05-19 16:13:59 +0800

    抽离side bar到单独的组件

---
+++ src/components/usercenter/SideBar.vue
@@ -0,0 +1,70 @@
+<template>
+  <Menu :active-name="activeItem" width="auto" v-on:on-select="handleMenuSelectChange">
+    <Menu-group title="信息管理">
+      <Menu-item name="1">
+        <Icon type="document-text"></Icon>
+        基本信息
+      </Menu-item>
+      <Menu-item name="2">
+        <Icon type="chatbubbles"></Icon>
+        修改密码
+      </Menu-item>
+    </Menu-group>
+    <Menu-group title="功能区">
+      <Menu-item name="3">
+        <Icon type="heart"></Icon>
+        我要当主播
+      </Menu-item>
+      <Menu-item name="4">
+        <Icon type="heart-broken"></Icon>
+        我的直播
+      </Menu-item>
+    </Menu-group>
+  </Menu>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        activeItem: '1'
+      }
+    },
+    created() {
+      this.getPath();
+    },
+    methods: {
+      handleMenuSelectChange(name) {
+        switch (name) {
+          case '1':
+            this.$router.push('/usercenter/profile');
+            break;
+          case '2':
+            this.$router.push('/usercenter/password');
+            break;
+          case '3':
+            this.doLogout();
+            break;
+          case '4':
+            this.$router.push('/usercenter');
+            break;
+        }
+      },
+      getPath() {
+        let path = this.$route.path;
+        switch (path) {
+          case '/usercenter/profile':
+            this.activeItem = '1';
+            break;
+          case '/usercenter/password':
+            this.activeItem = '2';
+            break;
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
--- src/components/usercenter/UserCenter.vue
+++ src/components/usercenter/UserCenter.vue
@@ -4,28 +4,7 @@
     <div class="main-area">
       <Row>
         <i-col span="5">
-          <Menu active-name="1" width="auto">
-            <Menu-group title="信息管理">
-              <Menu-item name="1">
-                <Icon type="document-text"></Icon>
-                基本信息
-              </Menu-item>
-              <Menu-item name="2">
-                <Icon type="chatbubbles"></Icon>
-                修改密码
-              </Menu-item>
-            </Menu-group>
-            <Menu-group title="功能区">
-              <Menu-item name="3">
-                <Icon type="heart"></Icon>
-                我要当主播
-              </Menu-item>
-              <Menu-item name="4">
-                <Icon type="heart-broken"></Icon>
-                我的直播
-              </Menu-item>
-            </Menu-group>
-          </Menu>
+          <side-bar></side-bar>
         </i-col>
         <i-col span="19" ref="layout">
           <router-view></router-view>
@@ -39,10 +18,12 @@
 <script>
   import TopNav from '../common/TopNav.vue';
   import vFooter from '../common/Footer.vue';
+  import SideBar from './SideBar.vue';
   export default {
     components: {
       TopNav,
-      vFooter
+      vFooter,
+      SideBar
     }
   }
 </script>
