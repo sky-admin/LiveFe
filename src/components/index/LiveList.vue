a9b0fcf82d473c8c4cf2d9b219669ca8c0ac8477
sky-ADMIN <546485299@qq.com>
2017-06-13 22:31:38 +0800

    update

---
+++ src/components/index/LiveList.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="live-list-container">
+    <Row>
+      <Col v-for="item in list" :key="item.clientId" :xs="24" :sm="8" :md="6" :lg="6">
+        {{item.name}}
+      </Col>
+    </Row>
+  </div>
+</template>
+
+<script>
+  import API from '../../config/request';
+  export default {
+    data() {
+      return {
+        list: []
+      }
+    },
+    mounted() {
+      this.getLiveList();
+    },
+    methods: {
+      getLiveList() {
+        this.$http.get(API.getLiveList).then(
+          (res) => {
+            console.log(res.data);
+            this.list = res.data;
+          }
+        )
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
--- src/config/request.js
+++ src/config/request.js
@@ -3,10 +3,10 @@
  */
 
 // 本地开发用
-// const baseUrl = 'http://localhost:3000/api/';
+const baseUrl = 'http://localhost:3000/api/';
 
 // 线上正式版
-const baseUrl = 'https://apis.lihuanyu.com/api/';
+// const baseUrl = 'https://apis.lihuanyu.com/api/';
 
 const API = {
   reg: baseUrl + 'Users',
@@ -14,7 +14,8 @@ const API = {
   user(userId, accessToken) {return baseUrl + 'Users/' + userId + '?access_token=' + accessToken},
   logout(accessToken) {return baseUrl + 'Users/logout?access_token=' + accessToken},
   changePassword(accessToken) {return baseUrl + 'Users/change-password?access_token=' + accessToken},
-  createLive: baseUrl + 'Lives'
+  createLive: baseUrl + 'Lives',
+  getLiveList: baseUrl + 'Lives'
 };
 
 module.exports = API;
