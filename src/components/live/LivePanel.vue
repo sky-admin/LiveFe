f93b21dae6ac24fabbecc8e9a66cd6a176737d64
sky-ADMIN <546485299@qq.com>
2017-06-14 14:21:28 +0800

    update

--- src/components/live/LivePanel.vue
+++ src/components/live/LivePanel.vue
@@ -4,16 +4,36 @@
       <div class="intro">
         <h1>直播人</h1>
       </div>
-      <video-player></video-player>
+      <video-player :player-config="playerConfig"></video-player>
     </div>
   </div>
 </template>
 
 <script>
   import VideoPlayer from '../common/VideoPlayer.vue';
+  import API from '../../config/request';
   export default {
     data() {
-      return {}
+      return {
+        playerConfig: {
+          width: 800,
+          height: 400,
+          file: ""
+        }
+      }
+    },
+    created() {
+      this.getLiveData();
+    },
+    methods: {
+      getLiveData() {
+        this.$http.get(API.getLiveById(this.$route.params.id)).then(
+          (res) => {
+            console.log(res.data);
+            this.playerConfig.file = res.data.rtmpPlay;
+          }
+        )
+      }
     },
     components: {
       VideoPlayer
